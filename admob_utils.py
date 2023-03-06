676228b7d8b4359206a2b746097045145cf3e404
Zain Imran <zainimran@outlook.com>
2023-03-06 15:34:55 +0800

    Add files via upload

---
+++ admob_utils.py
@@ -0,0 +1,185 @@
+# Copyright 2020 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import hashlib
+import os
+import re
+import socket
+import sys
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+
+# [START main_body]
+PORT = 8899
+REDIRECT_URI = f"http://127.0.0.1:{PORT}"
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+API_NAME = "admob"
+API_VERSION = "v1"
+API_SCOPE = "https://www.googleapis.com/auth/admob.readonly"
+
+# Store refresh tokens in a local disk file. This file contains sensitive
+# authorization information.
+TOKEN_FILE = 'token.pickle'
+
+
+def load_user_credentials():
+  # Name of a file containing the OAuth 2.0 information for this
+  # application, including client_id and client_secret, which are found
+  # on the Credentials tab on the Google Developers Console.
+  client_secrets = os.path.join(os.path.dirname(__file__), CLIENT_SECRETS_FILE)
+  return client_secrets
+
+
+# Authenticate user and create AdMob Service Object.
+def authenticate():
+  """Authenticates a user and creates an AdMob Service Object.
+
+  Returns:
+    An AdMob Service Object that is authenticated with the user using either
+    a client_secrets file or previously stored access and refresh tokens.
+  """
+
+  # The TOKEN_FILE stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists(TOKEN_FILE):
+    with open(TOKEN_FILE, 'rb') as token:
+      credentials = pickle.load(token)
+
+    if credentials and credentials.expired and credentials.refresh_token:
+      credentials.refresh(Request())
+
+  # If there are no valid stored credentials, authenticate using the
+  # client_secrets file.
+  else:
+    client_secrets = load_user_credentials()
+    flow = Flow.from_client_secrets_file(client_secrets, scopes=[API_SCOPE])
+
+    flow.redirect_uri = REDIRECT_URI
+
+    # Create an anti-forgery state token as described here:
+    # https://developers.google.com/identity/protocols/OpenIDConnect#createxsrftoken
+    passthrough_val = hashlib.sha256(os.urandom(1024)).hexdigest()
+
+    # Redirect the user to auth_url on your platform.
+    authorization_url, state = flow.authorization_url(
+        access_type="offline",
+        state=passthrough_val,
+        included_granted_scopes="true")
+
+    # Prints the authorization URL so you can paste into your browser. In a
+    # typical web application you would redirect the user to this URL, and they
+    # would be redirected back to "redirect_url" provided earlier after
+    # granting permission.
+    print("Paste this URL into your browser: ")
+    print(authorization_url)
+    print(f"\nWaiting for authorization and callback to: {REDIRECT_URI}...")
+
+    # Retrieves an authorization code by opening a socket to receive the
+    # redirect request and parsing the query parameters set in the URL.
+    code = _get_authorization_code(passthrough_val)
+
+    # Pass the code back into the OAuth module to get a refresh token.
+    flow.fetch_token(code=code)
+    refresh_token = flow.credentials.refresh_token
+    credentials = flow.credentials
+
+    print(f"\nYour refresh token is: {refresh_token}\n")
+
+    # Save the credentials for the next run.
+    with open("token.pickle", "wb") as token:
+      pickle.dump(credentials, token)
+
+  # Build the AdMob service.
+  admob = build(API_NAME, API_VERSION, credentials=credentials)
+  return admob
+
+
+def _get_authorization_code(passthrough_val):
+  """Opens a socket to handle a single HTTP request containing auth tokens.
+
+    Args:
+        passthrough_val: an anti-forgery token used to verify the request
+          received by the socket.
+
+    Returns:
+        a str access token from the Google Auth service.
+    """
+  # Open a socket at localhost:PORT and listen for a request
+  sock = socket.socket()
+  sock.bind(("localhost", PORT))
+  sock.listen(1)
+  connection, address = sock.accept()
+  data = connection.recv(1024)
+  # Parse the raw request to retrieve the URL query parameters.
+  params = _parse_raw_query_params(data)
+
+  try:
+    if not params.get("code"):
+      # If no code is present in the query params then there will be an
+      # error message with more details.
+      error = params.get("error")
+      message = f"Failed to retrieve authorization code. Error: {error}"
+      raise ValueError(message)
+    elif params.get("state") != passthrough_val:
+      message = "State token does not match the expected state."
+      raise ValueError(message)
+    else:
+      message = "Authorization code was successfully retrieved."
+  except ValueError as error:
+    print(error)
+    sys.exit(1)
+  finally:
+    response = ("HTTP/1.1 200 OK\n"
+                "Content-Type: text/html\n\n"
+                f"<b>{message}</b>"
+                "<p>Please check the console output.</p>\n")
+
+    connection.sendall(response.encode())
+    connection.close()
+
+  return params.get("code")
+
+
+def _parse_raw_query_params(data):
+  """Parses a raw HTTP request to extract its query params as a dict.
+
+    Note that this logic is likely irrelevant if you're building OAuth logic
+    into a complete web application, where response parsing is handled by a
+    framework.
+    Args:
+        data: raw request data as bytes.
+
+    Returns:
+        a dict of query parameter key value pairs.
+    """
+  # Decode the request into a utf-8 encoded string
+  decoded = data.decode("utf-8")
+  # Use a regular expression to extract the URL query parameters string
+  match = re.search("GET\s\/\?(.*) ", decoded)
+  params = match.group(1)
+  # Split the parameters to isolate the key/value pairs
+  pairs = [pair.split("=") for pair in params.split("&")]
+  # Convert pairs to a dict to make it easy to access the values
+  return {key: val for key, val in pairs}
+  # [END main_body]
---
+++ main.py
@@ -0,0 +1,258 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# generate_network_report.py
+
+import admob_utils
+from google.cloud import bigquery
+from google.api_core.exceptions import NotFound
+# import json
+from flatten_json import flatten
+import base64
+import functions_framework
+from datetime import datetime, date, timedelta
+import pytz
+import os
+import sys
+
+
+PUBLISHER_ID = os.environ.get('PUBLISHER_ID')
+
+
+def generate_network_report(service, backfill=False):
+    """Generates and prints a network report.
+
+    Args:
+      service: An AdMob Service Object.
+      backfill: A Boolean flag to indicate whether to backfill data or not.
+    """
+    # Set date range. AdMob API only supports the account default timezone and
+    # "America/Los_Angeles", see
+    # https://developers.google.com/admob/api/v1/reference/rest/v1/accounts.networkReport/generate
+    # for more information.
+    tz = pytz.timezone('America/Los_Angeles')
+    datetime_now = datetime.now(tz)
+
+    # Set dimensions.
+    dimensions = ['DATE', 'APP', 'COUNTRY']
+
+    # Set metrics.
+    metrics = ['ESTIMATED_EARNINGS', 'IMPRESSION_RPM', 'AD_REQUESTS', 'MATCH_RATE',
+            'MATCHED_REQUESTS', 'SHOW_RATE', 'IMPRESSIONS', 'IMPRESSION_CTR', 'CLICKS']
+
+    # Set sort conditions.
+    sort_conditions = {'dimension': 'DATE', 'order': 'ASCENDING'}
+
+    data = []
+
+    if not backfill:
+        end_date = datetime_now.date() - timedelta(days=1)
+        date_range = {
+            'start_date': {'year': end_date.year, 'month': end_date.month, 'day': end_date.day},
+            'end_date': {'year': end_date.year, 'month': end_date.month, 'day': end_date.day}
+        }
+
+        print(date_range)
+
+        # Create network report specifications.
+        report_spec = {
+            'date_range': date_range,
+            'dimensions': dimensions,
+            'metrics': metrics,
+            'sort_conditions': [sort_conditions],
+            'localization_settings': {'currency_code': 'USD'},
+        }
+
+        # Create network report request.
+        request = {'report_spec': report_spec}
+
+        # Execute network report request.
+        response = service.accounts().networkReport().generate(
+            parent='accounts/{}'.format(PUBLISHER_ID), body=request).execute()
+        
+        num_rows = int(response[-1]['footer']['matchingRowCount'])
+        data = response[1:-1]
+    else:
+        end_date = datetime_now.date() - timedelta(days=1)
+
+        # if the cloud scheduler is still going to run for yesterday at 02:00, then start the backfill from the day before yesterday (two days ago)
+        cloud_scheduler_time = datetime(year=datetime_now.year, month=datetime_now.month, day=datetime_now.day, hour=2, minute=0, tzinfo=tz)
+        if datetime_now < cloud_scheduler_time: 
+            end_date = datetime_now.date() - timedelta(days=2)
+
+        if (os.environ.get('START_DATE_YEAR') and os.environ.get('START_DATE_MONTH') and os.environ.get('START_DATE_DAY')):
+            start_date = date(int('20' + os.environ.get('START_DATE_YEAR')), int(os.environ.get('START_DATE_MONTH')), int(os.environ.get('START_DATE_DAY')))
+        else:
+            return
+
+        date_range = {
+            'start_date': {'year': start_date.year, 'month': start_date.month, 'day': start_date.day},
+            'end_date': {'year': end_date.year, 'month': end_date.month, 'day': end_date.day}
+        }
+
+        left, right = 1, (end_date - start_date).days + 1
+        while left <= right:
+            mid = (left + right) // 2
+            end_date_mid = start_date + timedelta(days=mid - 1)
+
+            date_range = {
+                'start_date': {'year': start_date.year, 'month': start_date.month, 'day': start_date.day},
+                'end_date': {'year': end_date_mid.year, 'month': end_date_mid.month, 'day': end_date_mid.day}
+            }
+
+            # Create network report specifications.
+            report_spec = {
+                'date_range': date_range,
+                'dimensions': dimensions,
+                'metrics': metrics,
+                'sort_conditions': [sort_conditions],
+                'localization_settings': {'currency_code': 'USD'},
+            }
+
+            # Create network report request.
+            request = {'report_spec': report_spec}
+
+            response = service.accounts().networkReport().generate(
+                parent='accounts/{}'.format(PUBLISHER_ID), body=request).execute()
+
+            # If the total number of records in the response is less than 100k,
+            # try a larger batch size; otherwise, try a smaller batch size
+            num_rows = int(response[-1]['footer']['matchingRowCount'])
+            if num_rows < 100000:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        # The optimal batch size is the largest batch size that returns less than 100k rows
+        batch_size = right
+        print('batch_size: ', batch_size, '\n')
+
+        # Loop through each batch of data, adjusting the date range as needed
+        offset = 0
+        while True:
+            # Calculate the start and end dates for the current batch
+            batch_start_date = start_date + timedelta(days=offset - 1)
+            batch_end_date = min(batch_start_date + timedelta(days=batch_size - 1), end_date)
+            
+            # Make the API request for the current batch
+            date_range = {
+                'start_date': {'year': batch_start_date.year, 'month': batch_start_date.month, 'day': batch_start_date.day},
+                'end_date': {'year': batch_end_date.year, 'month': batch_end_date.month, 'day': batch_end_date.day}
+            }
+
+            print(date_range)
+
+            # Create network report specifications.
+            report_spec = {
+                'date_range': date_range,
+                'dimensions': dimensions,
+                'metrics': metrics,
+                'sort_conditions': [sort_conditions],
+                'localization_settings': {'currency_code': 'USD'},
+            }
+
+            # Create network report request.
+            request = {'report_spec': report_spec}
+
+            response = service.accounts().networkReport().generate(
+                parent='accounts/{}'.format(PUBLISHER_ID), body=request).execute()
+
+            num_rows = int(response[-1]['footer']['matchingRowCount'])
+            print('batch num_rows: ', num_rows, '\n')
+
+            # Append the data from the current batch to the overall data list
+            data.extend(response[1:-1])
+            
+            # Increment the offset by the number of days in the current batch
+            offset += batch_size
+            
+            # If the end date of the current batch is equal to the overall end date,
+            # we have retrieved all the data and can break out of the loop
+            if batch_end_date == end_date:
+                break
+        
+
+    # Flatten the json and filter out empty country rows.
+    idx = 0
+    data_length = len(data)
+    while idx < data_length:
+        data[idx] = flatten(data[idx]['row'])
+        if 'dimensionValues_COUNTRY' in data[idx]: # after flattening the data json, dimensionValues_COUNTRY will only exist if dimensionValues_COUNTRY_value does not exist
+            data.pop(idx)
+            data_length -= 1
+        else:
+            idx += 1
+
+    # # Test out by saving the data to a local json file.
+    # json_string = json.dumps(data)
+    # with open('json_data.json', 'w') as outfile:
+    #     outfile.write(json_string)
+
+    # Construct a BigQuery client object.
+    # TODO(developer): Set project to the project ID where the BigQuery table is located.
+    client = bigquery.Client(project=os.environ.get('PROJECT_ID'))
+
+    # TODO(developer): Set table_id to the ID of the table to create.
+    table_id = os.environ.get('TABLE_ID')
+
+    # Check if table exists
+    table_exists = False
+    try:
+        client.get_table(table_id)
+        table_exists = True
+    except NotFound:
+        pass
+
+    # Create table if it doesn't exist
+    if not table_exists:
+        table = bigquery.Table(table_id)
+        table = client.create_table(table)
+        print(f"Table {table.table_id} created.")
+    else:
+        print(f"Table {table_id} already exists.")
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON, autodetect=True)
+
+    job = client.load_table_from_json(
+        data, table_id, job_config=job_config)
+
+    job.result()  # Waits for the job to complete.
+
+    table = client.get_table(table_id)  # Make an API request.
+    print(
+        "Loaded {} rows and {} columns to {}".format(
+            table.num_rows, len(table.schema), table_id
+        )
+    )
+
+
+# Triggered from a message on a Cloud Pub/Sub topic.
+@functions_framework.cloud_event
+def admob_report_main(cloud_event):
+    service = admob_utils.authenticate()
+    # # Print out the data from Pub/Sub, to prove that it worked
+    message = base64.b64decode(cloud_event.data["message"]["data"]).decode() 
+    print("Message: " + message)
+    backfill = False
+    if (message == 'backfill' or message == 'Backfill' or message == 'BACKFILL'):
+        backfill = True
+    generate_network_report(service, backfill)
+
+
+if __name__ == "__main__":
+    service = admob_utils.authenticate()
+    backfill = False
+    if len(sys.argv) - 1:
+        backfill = True
+    generate_network_report(service, backfill)
