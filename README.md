0569383849d94f2391bc6df0aff146c3bfa580ec
Zain Imran <zainimran@outlook.com>
2023-03-07 12:34:04 +0800

    Add readme file, gitignore and run.sh shell script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-
+# Steps
+1. Fill client_id, project_id, and client_secret in the client_secrets.json file first before running `./run.sh`
+2. Run `chmod +x run.sh` in terminal
+3. Now, run `./run.sh` in terminal as well
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create virtual environment
+python3 -m venv .venv
+
+# activate the virtual environment
+source .venv/bin/activate
+
+# install dependencies
+pip3 install -r requirements.txt
+
+# run main script
+python3 main.py
+
+# deactivate the virtual environment
+deactivate
